159bfe3c1b0a6abffbd5138cc313e00345fc4490
JanneP80 <janne.puolitaival@student.saimia.fi>
2017-03-21 14:53:13 +0200

    web api

---
+++ users-repo/server-project/WebApi/WebApi/Models/WebContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class WebContext : DbContext
+    {
+        public WebContext(DbContextOptions<WebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebItem> TodoItems { get; set; }
+    }
+}
